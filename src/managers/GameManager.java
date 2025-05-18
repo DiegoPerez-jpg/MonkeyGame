c0b3695074acc778e9aea28a3a14a7fbb76e654e
Mario <mario.m.g.5557@gmail.com>
2025-05-18 19:37:28 +0200

    gm update

--- src/managers/GameManager.java
+++ src/managers/GameManager.java
@@ -1,11 +1,33 @@
 package managers;
 
+import graphics.Renderer;
+
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+
 public class GameManager {
     private static GameManager instance;
-    private GameManager(){}
+    public InputManager inputManager;
+    public int width, height;
+    public Renderer renderer;
+
+    private GameManager(){
+        this.width = 800;
+        this.height = 600;
+    }
 
     public static GameManager getInstance() {
         if (instance == null) {instance = new GameManager();}
         return instance;
+    }
+
+    public void play(){
+        //Estas clases llaman al gm en sus constructores por lo que no pueden ser creadas en el contructor del gm
+        this.renderer = new Renderer(width, height);
+        this.inputManager = new InputManager();
+        //glfwWindowShouldClose devuelve true si se cierra la ventana
+        while (!glfwWindowShouldClose(renderer.getWindow())) { //Game loop
+            renderer.update();
+        }
+        renderer.clean();
     }
 }
