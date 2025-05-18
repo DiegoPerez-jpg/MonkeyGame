0ff56b5705e5f819cae052138a85ab01c097d717
Diego Pérez <diegopg2005@gmail.com>
2025-05-18 23:07:10 +0200

    baloon manager

---
+++ src/managers/BalloonManager.java
@@ -0,0 +1,21 @@
+package managers;
+
+import entities.balloons.Balloon;
+
+import java.util.ArrayList;
+
+public class BalloonManager {
+    ArrayList<Balloon> balloons = new ArrayList<Balloon>();
+    public BalloonManager() {
+        balloons = new ArrayList();
+    }
+    public void addBalloon(Balloon balloon) {
+        balloons.add(balloon);
+    }
+    public void removeBalloon(Balloon balloon) {
+        balloons.remove(balloon);
+    }
+    public ArrayList<Balloon> getBalloons() {
+        return balloons;
+    }
+}
